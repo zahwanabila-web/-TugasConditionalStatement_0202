ee7fbf1eb9a2f763d522e86bc5da35e218247b6b
ZahwaNabila <zahwanabila434@gmail.com>
2026-03-10 00:36:19 +0700

    commit pertama

---
+++ pemrograman.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+// prosedur untuk memasukkan data
+void input(float &berat, float &tinggi){
+    cout << "Masukkan berat badan : ";
+    cin >> berat;
+}
